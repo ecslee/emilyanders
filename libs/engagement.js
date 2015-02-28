c3dbcfbc7ad7cbe128504108c33dd8db57b39010
Emily Seitz <emily.seitz@pearson.com>
2015-02-28 01:12:35 -0600

    center on Warrenton to show Luray vs DC

--- libs/engagement.js
+++ libs/engagement.js
@@ -170,14 +170,17 @@ $(document).ready(function () {
         var khimairaLatLong = new google.maps.LatLng(38.628217, -78.428725);
         var mapOptions = {
             zoom: 8,
-            center: khimairaLatLong
+            // Warrenton, VA
+            center: new google.maps.LatLng(38.729663, -77.792662)
         };
         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
         
         var khimaira = new google.maps.Marker({
             position: khimairaLatLong,
             map: map,
-            title: 'Khimaira Farm'
+            title: 'Khimaira Farm',
+            icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=1|4A2544|FFFFFF',
+            zIndex: 3
         });
         var khimairaInfo = new google.maps.InfoWindow({
             content: '<div><h4>Khimaira Farm</h4><p>2974 Stonyman Rd<br>Luray, VA 22835</p></div>'
@@ -186,7 +189,9 @@ $(document).ready(function () {
         var mimslyn = new google.maps.Marker({
             position: new google.maps.LatLng(38.664417, -78.466819),
             map: map,
-            title: 'Mimslyn Inn'
+            title: 'Mimslyn Inn',
+            icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=2|4A2544|FFFFFF',
+            zIndex: 2
         });
         var mimslynInfo = new google.maps.InfoWindow({
             content: '<div><h4>Mimslyn Inn</h4><p>401 W. Main Street<br>Luray, VA 22835</p></div>'
@@ -195,7 +200,9 @@ $(document).ready(function () {
         var caverns = new google.maps.Marker({
             position: new google.maps.LatLng(38.663911, -78.483257),
             map: map,
-            title: 'Luray Caverns'
+            title: 'Luray Caverns',
+            icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=3|4A2544|FFFFFF',
+            zIndex: 1
         });
         var cavernsInfo = new google.maps.InfoWindow({
             content: '<div><h4>Luray Caverns</h4><p>101 Cave Hill Rd<br>Luray, VA 22835</p></div>'
@@ -203,14 +210,17 @@ $(document).ready(function () {
         
         google.maps.event.addListener(khimaira, 'click', function() {
             mimslynInfo.close();
+            cavernsInfo.close();
             khimairaInfo.open(map, khimaira);
         });
         google.maps.event.addListener(mimslyn, 'click', function() {
             khimairaInfo.close();
+            cavernsInfo.close();
             mimslynInfo.open(map, mimslyn);
         });
         google.maps.event.addListener(caverns, 'click', function() {
-            cavernsInfo.close();
+            khimairaInfo.close();
+            mimslynInfo.close();
             cavernsInfo.open(map, caverns);
         });
     }

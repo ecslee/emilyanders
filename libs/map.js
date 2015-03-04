fbb38bb38e9ee9a823c1e095fdcad0fa4c0590d6
Emily Seitz <emily.seitz@pearson.com>
2015-03-04 00:29:12 -0600

    map icons respond to accordion clicks

--- libs/map.js
+++ libs/map.js
@@ -1,72 +1,130 @@
 $(document).ready(function () {
     /* map accordion */
-    $('.panel-heading').click(function () {
+    $('.panel-heading', '.panel-wedding').click(function () {
         if ($(this).next().hasClass('panel-open')) {
             return;
         }
         
-        $('.panel-body').removeClass('panel-open', 500, 'easeInOutCubic');
+        $('.panel-body', '.panel-wedding').removeClass('panel-open', 500, 'easeInOutCubic');
         $(this).next().addClass('panel-open', 500, 'easeInOutCubic');
     });
 
     /* google map */
     var map;
     function mapInit() {
+        var icons = {
+            purple: {
+                khimaira: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=1|4A2544|FFFFFF',
+                hotel: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=2|4A2544|FFFFFF',
+                caverns: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=3|4A2544|FFFFFF'
+            },
+            blue: {
+                khimaira: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=1|9AD0FF|444444',
+                hotel: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=2|9AD0FF|444444',
+                caverns: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=3|9AD0FF|444444'
+            }
+        };
+        
         var khimairaLatLong = new google.maps.LatLng(38.628217, -78.428725);
         var mapOptions = {
-            zoom: 8,
-            // Warrenton, VA - recenter on page resize
-            center: new google.maps.LatLng(38.729663, -77.792662)
+            zoom: 9,
+            center: new google.maps.LatLng(38.762418, -78.279751)
+            //center: khimairaLatLong
         };
         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-        var khimaira = new google.maps.Marker({
+        var khimairaMarker = new google.maps.Marker({
             position: khimairaLatLong,
             map: map,
             title: 'Khimaira Farm',
-            icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=1|4A2544|FFFFFF',
-            zIndex: 3
+            icon: icons.purple.khimaira,
+            zIndex: 3,
+            weddingType: 'khimaira'
         });
         var khimairaInfo = new google.maps.InfoWindow({
             content: '<div><h4>Khimaira Farm</h4><p>2974 Stonyman Rd<br>Luray, VA 22835</p></div>'
         });
 
-        var mimslyn = new google.maps.Marker({
+        var mimslynMarker = new google.maps.Marker({
             position: new google.maps.LatLng(38.664417, -78.466819),
             map: map,
             title: 'Mimslyn Inn',
-            icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=2|4A2544|FFFFFF',
-            zIndex: 2
+            icon: icons.blue.hotel,
+            zIndex: 2,
+            weddingType: 'hotel'
         });
         var mimslynInfo = new google.maps.InfoWindow({
             content: '<div><h4>Mimslyn Inn</h4><p>401 W. Main Street<br>Luray, VA 22835</p></div>'
         });
+        
+        var hamptonMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(38.960593, -78.194782),
+            map: map,
+            title: 'Hampton Inn',
+            icon: icons.blue.hotel,
+            zIndex: 2,
+            weddingType: 'hotel'
+        });
+        var hamptonInfo = new google.maps.InfoWindow({
+            content: '<div><h4>Mimslyn Inn</h4><p>401 W. Main Street<br>Luray, VA 22835</p></div>'
+        });
 
-        var caverns = new google.maps.Marker({
+        var cavernsMarker = new google.maps.Marker({
             position: new google.maps.LatLng(38.663911, -78.483257),
             map: map,
             title: 'Luray Caverns',
-            icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=3|4A2544|FFFFFF',
-            zIndex: 1
+            icon: icons.blue.caverns,
+            zIndex: 1,
+            weddingType: 'caverns'
         });
         var cavernsInfo = new google.maps.InfoWindow({
             content: '<div><h4>Luray Caverns</h4><p>101 Cave Hill Rd<br>Luray, VA 22835</p></div>'
         });
 
-        google.maps.event.addListener(khimaira, 'click', function () {
+        google.maps.event.addListener(khimairaMarker, 'click', function () {
             mimslynInfo.close();
             cavernsInfo.close();
-            khimairaInfo.open(map, khimaira);
+            khimairaInfo.open(map, khimairaMarker);
         });
-        google.maps.event.addListener(mimslyn, 'click', function () {
+        google.maps.event.addListener(mimslynMarker, 'click', function () {
             khimairaInfo.close();
             cavernsInfo.close();
-            mimslynInfo.open(map, mimslyn);
+            mimslynInfo.open(map, mimslynMarker);
         });
-        google.maps.event.addListener(caverns, 'click', function () {
+        google.maps.event.addListener(cavernsMarker, 'click', function () {
             khimairaInfo.close();
             mimslynInfo.close();
-            cavernsInfo.open(map, caverns);
+            cavernsInfo.open(map, cavernsMarker);
+        });
+        
+        var markers = [
+            khimairaMarker,
+            mimslynMarker,
+            hamptonMarker,
+            cavernsMarker
+        ];
+        $('.panel-heading', '.panel-wedding').click(function () {
+            var m;
+            for (m=0; m<markers.length; m++) {
+                markers[m]['z-index'] = 1;
+                markers[m].setIcon(icons.blue[markers[m].weddingType]);
+            }
+            switch (this.id) {
+                case 'khimairaPanel':
+                    khimairaMarker.setIcon(icons.purple.khimaira);
+                    khimairaMarker['z-index'] = 3;
+                    break;
+                case 'hotelsPanel':
+                    mimslynMarker.setIcon(icons.purple.hotel);
+                    hamptonMarker.setIcon(icons.purple.hotel);
+                    mimslynMarker['z-index'] = 3;
+                    hamptonMarker['z-index'] = 3;
+                    break;
+                case 'cavernsPanel':
+                    cavernsMarker.setIcon(icons.purple.caverns);
+                    cavernsMarker['z-index'] = 3;
+                    break;
+            }
         });
     }
 
